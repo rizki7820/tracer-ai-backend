b37f5105794fe680757f971b78d721d38e4569c2
rizki7820 <rizki7820@gmail.com>
2026-07-06 09:35:07 +0800

    Phase 1

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,9 +17,14 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role',[
+            'admin',
+            'alumni',
+            'company'
+            ])->default('alumni');
             $table->rememberToken();
             $table->timestamps();
-        });
+            });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
