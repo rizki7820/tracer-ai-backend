9d3607853d331de264fbaccacfc6cd33eb7d279c
rizki7820 <rizki7820@gmail.com>
2026-09-04 17:05:57 +0800

    test lagi 2

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
 
-require_once __DIR__ . '/../public/index.php';
+// Menghubungkan ke autoloader bawaan Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Menjalankan aplikasi Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
