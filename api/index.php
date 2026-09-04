78c48223c7134dcf65438d5153f26069774601aa
rizki7820 <rizki7820@gmail.com>
2026-09-04 17:23:39 +0800

    day 1 di prank chatgpt 3

--- api/index.php
+++ api/index.php
@@ -2,10 +2,10 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-echo "AUTOLOAD OK";
-exit;
-
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+echo "BOOTSTRAP OK";
+exit;
 
 $app->handleRequest(
     Illuminate\Http\Request::capture()
