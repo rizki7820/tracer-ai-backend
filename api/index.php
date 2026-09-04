2250b30bdd705d52286930b750f0865dc534c7b9
rizki7820 <rizki7820@gmail.com>
2026-09-04 17:21:28 +0800

    day 1 di prank chatgpt

--- api/index.php
+++ api/index.php
@@ -1,17 +1,6 @@
 <?php
 
-// Menghubungkan ke autoloader bawaan Composer
+echo "API PHP OK";
+exit;
+
 require __DIR__ . '/../vendor/autoload.php';
-
-// Menjalankan aplikasi Laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
