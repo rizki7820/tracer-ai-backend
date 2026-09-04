c799b7fc14c42ade98473ba71eb11851b9757b23
rizki7820 <rizki7820@gmail.com>
2026-09-04 17:30:35 +0800

    day 1 di prank chatgpt 5

--- api/index.php
+++ api/index.php
@@ -4,6 +4,18 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$app->handleRequest(
-    Illuminate\Http\Request::capture()
-);
+$request = Illuminate\Http\Request::capture();
+
+try {
+    $response = $app->handleRequest($request);
+    $response->send();
+} catch (\Throwable $e) {
+    http_response_code(500);
+
+    echo '<pre>';
+    echo get_class($e) . "\n\n";
+    echo $e->getMessage() . "\n\n";
+    echo $e->getFile() . ':' . $e->getLine() . "\n\n";
+    echo $e->getTraceAsString();
+    echo '</pre>';
+}
